837c3732acfe3e913cc53103649179aea7358c41
mm51593 <mm51593@github.com>
2026-06-05 10:59:50 +0200

    Add lazy.nvim

---
+++ lua/options/init.lua
@@ -0,0 +1,17 @@
+vim.opt.number = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.termguicolors = true
+
+vim.opt.signcolumn = "yes"
+
+vim.opt.clipboard = "unnamedplus"
