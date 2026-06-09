a6f15b140d2d478f57872cd9ccc96a21af4edd7c
mm51593 <mm51593@github.com>
2026-06-09 09:50:46 +0200

    Add lazydev

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -4,6 +4,19 @@ return {
 
         dependencies = {
             "rafamadriz/friendly-snippets",
+            {
+                "folke/lazydev.nvim",
+                ft = "lua", -- only load on lua files
+                opts = {
+                    library = {
+                        -- See the configuration section for more details
+                        --
+                        --         -- Load luvit types when the `vim.uv` word is found
+                        --
+                        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+                    },
+                },
+            },
         },
 
         version = "*",
@@ -29,6 +42,15 @@ return {
                     "path",
                     "snippets",
                     "buffer",
+                    "lazydev",
+                },
+                providers = {
+                    lazydev = {
+                        name = "LazyDev",
+                        module = "lazydev.integrations.blink",
+
+                        score_offset = 100,
+                    },
                 },
             },
         },
