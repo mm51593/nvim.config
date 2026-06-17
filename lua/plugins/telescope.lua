ea4d4b78862d89245c9664e5eac644b597dfd9d4
mm51593 <mm51593@github.com>
2026-06-17 14:33:20 +0200

    Telescope: update find_files and diagnostics

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,7 +5,7 @@ return {
         dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-telescope/telescope-fzf-native.nvim",
-            "nvim-telescope/telescope-ui-select.nvim"
+            "nvim-telescope/telescope-ui-select.nvim",
         },
         build = "make",
 
@@ -30,7 +30,15 @@ return {
             })
             local builtin = require("telescope.builtin")
 
-            vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "Find files" })
+            local find_files_noignore = function()
+                builtin.find_files({ no_ignore = true })
+            end
+
+            local diagnostics_one_file = function()
+                builtin.diagnostics({ buffnr = 0 })
+            end
+
+            vim.keymap.set("n", "<leader>sf", find_files_noignore, { desc = "Find files" })
             vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Live grep" })
             vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "Buffers" })
             vim.keymap.set("n", "<leader>sh", builtin.help_tags, { desc = "Help" })
@@ -40,6 +48,7 @@ return {
             vim.keymap.set("n", "gd", builtin.lsp_definitions, { desc = "Goto definition" })
             vim.keymap.set("n", "gr", builtin.lsp_references, { desc = "Goto references" })
             vim.keymap.set("n", "gi", builtin.lsp_implementations, { desc = "Goto implementations" })
+            vim.keymap.set("n", "go", builtin.lsp_outgoing_calls, { desc = "Goto outgoing" })
             vim.keymap.set("n", "<leader>ds", builtin.lsp_document_symbols, { desc = "Document symbols" })
             vim.keymap.set("n", "<leader>ws", builtin.lsp_dynamic_workspace_symbols, { desc = "Workspace symbols" })
 
@@ -47,7 +56,7 @@ return {
             vim.keymap.set("n", "<leader>gb", builtin.git_branches, { desc = "Git branches" })
             vim.keymap.set("n", "<leader>gs", builtin.git_status, { desc = "Git status" })
 
-            vim.keymap.set('n', '<leader>q', builtin.diagnostics, { desc = 'Open diagnostics list' })
+            vim.keymap.set('n', '<leader>q', diagnostics_one_file, { desc = 'Open diagnostics list' })
         end,
     },
 }
